fda14fcf13b676c6702bfc7765e224c0d68bc9bf
kyuung09 <kyuung09@gmail.com>
2022-11-30 03:06:28 +0900

    Blog Request Dto

--- src/main/java/com/example/hanghaeblogprac/dto/BlogRequestDto.java
+++ src/main/java/com/example/hanghaeblogprac/dto/BlogRequestDto.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public class BlogRequestDto {
+    private String title;
     private String username;
     private String contents;
     private String password;
-
 }
