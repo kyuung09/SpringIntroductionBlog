19caa056799af24dcede1bd6693703bbf8d9e619
kyuung09 <kyuung09@gmail.com>
2022-11-30 03:07:34 +0900

    Blog Controller Update

--- src/main/java/com/example/hanghaeblogprac/controller/BlogController.java
+++ src/main/java/com/example/hanghaeblogprac/controller/BlogController.java
@@ -1,14 +1,14 @@
 package com.example.hanghaeblogprac.controller;
 
+import com.example.hanghaeblogprac.dto.BlogListResponseDto;
 import com.example.hanghaeblogprac.dto.BlogRequestDto;
-import com.example.hanghaeblogprac.entity.Blog;
+import com.example.hanghaeblogprac.dto.BlogResponseDto;
+import com.example.hanghaeblogprac.dto.ResponseDto;
 import com.example.hanghaeblogprac.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,41 +16,33 @@ public class BlogController {
 
     private final BlogService blogService;
 
-
     @GetMapping("/")
     public ModelAndView home() {
         return new ModelAndView("index");
     }
 
     @PostMapping("/api/blogs")
-    public Blog createBlog(@RequestBody BlogRequestDto requestDto){
+    public BlogResponseDto createBlog(@RequestBody BlogRequestDto requestDto) {
         return blogService.createBlog(requestDto);
     }
 
     @GetMapping("/api/blogs")
-    public List<Blog> getBlogs() {
+    public BlogListResponseDto getBlogs() {
         return blogService.getBlogs();
     }
 
     @GetMapping("/api/blogs/{id}")
-    public Optional<Blog> getBlogsOne(@PathVariable Long id, BlogRequestDto requestDto) {
-        return blogService.getBlogsOne(id, requestDto);
+    public BlogResponseDto getBlogsOne(@PathVariable Long id) {
+        return blogService.getBlogsOne(id);
     }
 
     @PutMapping("/api/blogs/{id}")
-    public long updateBlog(@PathVariable Long id, @RequestBody BlogRequestDto reqeustDto){
+    public BlogResponseDto updateBlog(@PathVariable Long id, @RequestBody BlogRequestDto reqeustDto) {
         return blogService.update(id, reqeustDto);
     }
 
     @DeleteMapping("/api/blogs/{id}")
-    public Long deleteBlog(@PathVariable Long id, @RequestBody BlogRequestDto requestDto){
+    public ResponseDto deleteBlog(@PathVariable Long id, @RequestBody BlogRequestDto requestDto) {
         return blogService.deleteBlog(id, requestDto);
     }
-
-
-//    @PostMapping("/api/blogs/{id}")
-//    public boolean comparePassword (@PathVariable Long id,@RequestBody BlogRequestDto.BlogPasswordDto BlogPasswordDto) {
-//        return blogService.comparePassword(id, BlogPasswordDto);
-//    }
-
 }
