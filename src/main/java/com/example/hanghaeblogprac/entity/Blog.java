4f9d749a9f40749c6bef8d4747e09eb30313447c
kyuung09 <kyuung09@gmail.com>
2022-11-29 17:13:20 +0900

    Timestamped Entity Class

---
+++ src/main/java/com/example/hanghaeblogprac/entity/Blog.java
@@ -0,0 +1,48 @@
+package com.example.hanghaeblogprac.entity;
+
+
+import com.example.hanghaeblogprac.dto.BlogRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Blog extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Blog(BlogRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+        this.password = requestDto.getPassword();
+    }
+
+    public void deleteBlog(BlogRequestDto reqeustDto) {
+        this.username = reqeustDto.getUsername();
+        this.password = reqeustDto.getPassword();
+    }
+
+    public void update(BlogRequestDto reqeustDto) {
+        this.username = reqeustDto.getUsername();
+        this.contents = reqeustDto.getContents();
+        this.password = reqeustDto.getPassword();
+    }
+
+    public void comparePassword(BlogRequestDto requestDto){
+        this.password = requestDto.getPassword();
+    }
+
+}
