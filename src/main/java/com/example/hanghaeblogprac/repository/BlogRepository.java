1b5493988199a63427d5cc4be8e700bffc069484
kyuung09 <kyuung09@gmail.com>
2022-11-30 03:02:28 +0900

    Blog Repository Setup

--- src/main/java/com/example/hanghaeblogprac/repository/BlogRepository.java
+++ src/main/java/com/example/hanghaeblogprac/repository/BlogRepository.java
@@ -1,16 +1,12 @@
 package com.example.hanghaeblogprac.repository;
 
 import com.example.hanghaeblogprac.entity.Blog;
-import org.junit.jupiter.api.AfterEach;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
-    List<Blog> findAllByOrderByModifiedAtDesc();
-    Optional<Blog> findByIdAndUsernameAndPassword(Long id, String Username, String password);
-
+    List<Blog> findAllByOrderByCreatedAtAsc();
     Optional<Blog> findByIdAndPassword(Long id, String password);
 
 }
