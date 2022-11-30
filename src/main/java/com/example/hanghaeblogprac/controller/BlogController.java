d2b698c360620672d27c1d439280dcaf030d126e
kyuung09 <kyuung09@gmail.com>
2022-12-01 02:38:37 +0900

    Blog Controller Update

--- src/main/java/com/example/hanghaeblogprac/controller/BlogController.java
+++ src/main/java/com/example/hanghaeblogprac/controller/BlogController.java
@@ -7,7 +7,6 @@ import com.example.hanghaeblogprac.dto.ResponseDto;
 import com.example.hanghaeblogprac.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
@@ -16,31 +15,31 @@ public class BlogController {
 
     private final BlogService blogService;
 
-    @GetMapping("/")
-    public ModelAndView home() {
-        return new ModelAndView("index");
-    }
-
+    // Blog posting DB Save
     @PostMapping("/api/blogs")
     public BlogResponseDto createBlog(@RequestBody BlogRequestDto requestDto) {
         return blogService.createBlog(requestDto);
     }
 
+    // DB Select all
     @GetMapping("/api/blogs")
     public BlogListResponseDto getBlogs() {
         return blogService.getBlogs();
     }
 
+    // DB Select One
     @GetMapping("/api/blogs/{id}")
     public BlogResponseDto getBlogsOne(@PathVariable Long id) {
         return blogService.getBlogsOne(id);
     }
 
+    // Blog posting DB Update
     @PutMapping("/api/blogs/{id}")
     public BlogResponseDto updateBlog(@PathVariable Long id, @RequestBody BlogRequestDto reqeustDto) {
         return blogService.update(id, reqeustDto);
     }
 
+    // Blog posting DB Delete
     @DeleteMapping("/api/blogs/{id}")
     public ResponseDto deleteBlog(@PathVariable Long id, @RequestBody BlogRequestDto requestDto) {
         return blogService.deleteBlog(id, requestDto);
