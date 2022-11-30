9291c88ecc5700607e7f9062fe99c2669bab322c
kyuung09 <kyuung09@gmail.com>
2022-12-01 02:38:50 +0900

    Blog ListResponseDto Update

--- src/main/java/com/example/hanghaeblogprac/dto/BlogListResponseDto.java
+++ src/main/java/com/example/hanghaeblogprac/dto/BlogListResponseDto.java
@@ -7,9 +7,8 @@ import java.util.List;
 
 @Getter
 public class BlogListResponseDto {
-    List<BlogResponseDto> blogList = new ArrayList<>();
+    List<BlogResponseDto> blogList = new ArrayList<>();     // 블로그 게시글 리스트 생성
     public void addBlog(BlogResponseDto blogResponseDto){
-        blogList.add(blogResponseDto);
+        blogList.add(blogResponseDto);                      // ResponseDto에 담기위한 add function
     }
-
 }
