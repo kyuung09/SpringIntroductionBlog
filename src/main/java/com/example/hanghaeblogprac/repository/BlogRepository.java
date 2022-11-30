617977d945419e5c5b85caa55af951d51c35e6b3
kyuung09 <kyuung09@gmail.com>
2022-12-01 02:39:00 +0900

    Blog Repository Update

--- src/main/java/com/example/hanghaeblogprac/repository/BlogRepository.java
+++ src/main/java/com/example/hanghaeblogprac/repository/BlogRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
-    List<Blog> findAllByOrderByCreatedAtAsc();
-    Optional<Blog> findByIdAndPassword(Long id, String password);
+    List<Blog> findAllByOrderByCreatedAtAsc();      // Select All 관련 리스트
 
+    Optional<Blog> findByIdAndPassword(Long id, String password);   // ID,PW 비교를 위한 Optional
 }
